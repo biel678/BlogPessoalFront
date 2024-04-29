d086fc17c9f2c433d158505173cf0568852d347c
biel678 <bieltomarchio@hotmail.com>
2024-04-29 15:18:32 -0300

    Adicionando o Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
 import './App.css';
 import Home from './paginas/Home/Home';
+import Navbar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login/Login';
 
 function App() {
   return (
     <>
-    <Home/>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
 );
 }
+
 export default App;
--- src/paginas/Home/Home.tsx
+++ src/paginas/Home/Home.tsx
@@ -1,38 +1,58 @@
-
+import homeLogo from '../../assets/home.jpeg'
 import './Home.css';
 
-function Manutencao(){
-    alert("Site em manutenção, em breve novas funcionalidades")
-}
+// function Home() {
+//     return (
 
+//         <div className=''>
+//             <div className='font-bold text-center text-4xl'>
+//                 Bem vinde ao Confy Blog!
+//             </div>
+
+//             <div className='text-center text-4xl mt-4 mb-5'>
+//                 Escolha o que você deseja fazer:
+//             </div>
+//             <div className='grid grid-cols-12 text-xl'>
+//                 <div className='col-span-4 text-center'>
+//                     <h1 className='text-2xl'>Criar:</h1>
+//                     <button className='border-4  border-black m-5'> Fazer postagem</button>
+//                 </div>
+//                 <div className='col-span-4 text-center'>
+//                     <h1 className='text-2xl'>Relaxar:</h1>
+//                     <button className='border-4  border-black m-5'> Fazer postagem</button>
+//                 </div>
+//                 <div className='col-span-4 text-center'>
+//                     <h1 className='text-2xl'>Procurar:</h1>
+//                     <button className='border-4  border-black m-5'> Fazer postagem</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 function Home() {
     return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+                        <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
 
-        <div className=''>
-            <div className='font-bold text-center text-4xl'>
-                Bem vinde ao Confy Blog!
+                        <div className="flex justify-around gap-4">
+
+                            <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img src={homeLogo} alt="" className='w-2/3' />
+
+                    </div>
+                </div>
             </div>
 
-            <div className='text-center text-4xl mt-4 mb-5'>
-                Escolha o que você deseja fazer:
-            </div>
-            <div className='grid grid-cols-12 text-xl'>
-                <div className='col-span-4 text-center'>
-                    <h1 className='text-2xl'>Criar:</h1>
-                    <button className='border-4  border-black m-5'> Fazer postagem</button>
-                </div>
-                <div className='col-span-4 text-center'>
-                    <h1 className='text-2xl'>Relaxar:</h1>
-                    <button className='border-4  border-black m-5'> Fazer postagem</button>
-                </div>
-                <div className='col-span-4 text-center'>
-                    <h1 className='text-2xl'>Procurar:</h1>
-                    <button className='border-4  border-black m-5'> Fazer postagem</button>
-                </div>
-            </div>
-        </div>
-    );
+        </>
+    )
 }
-
 
 export default Home;
