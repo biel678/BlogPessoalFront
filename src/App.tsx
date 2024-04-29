8e3529ac6b91a50dec6ecc55be3bf34a90e34eb2
biel678 <bieltomarchio@hotmail.com>
2024-04-29 12:41:46 -0300

    Adicionando estilo com TailWind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import './App.css';
 import Home from './paginas/Home/Home';
-
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <Home/>
+    </>
+);
 }
-
 export default App;
--- src/paginas/Home/Home.tsx
+++ src/paginas/Home/Home.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+
 import './Home.css';
 
-function Home(){
+function Manutencao(){
+    alert("Site em manutenção, em breve novas funcionalidades")
+}
+
+function Home() {
     return (
-        <>
-            <h1 >Home</h1>
-           
-        </>
+
+        <div className=''>
+            <div className='font-bold text-center text-4xl'>
+                Bem vinde ao Confy Blog!
+            </div>
+
+            <div className='text-center text-4xl mt-4 mb-5'>
+                Escolha o que você deseja fazer:
+            </div>
+            <div className='grid grid-cols-12 text-xl'>
+                <div className='col-span-4 text-center'>
+                    <h1 className='text-2xl'>Criar:</h1>
+                    <button className='border-4  border-black m-5'> Fazer postagem</button>
+                </div>
+                <div className='col-span-4 text-center'>
+                    <h1 className='text-2xl'>Relaxar:</h1>
+                    <button className='border-4  border-black m-5'> Fazer postagem</button>
+                </div>
+                <div className='col-span-4 text-center'>
+                    <h1 className='text-2xl'>Procurar:</h1>
+                    <button className='border-4  border-black m-5'> Fazer postagem</button>
+                </div>
+            </div>
+        </div>
     );
 }
+
 
 export default Home;
